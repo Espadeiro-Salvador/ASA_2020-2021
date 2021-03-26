6ba887e1423ecce1b7e45967c49bcfa67a18d922
Guilherme Salvador <guilherme.salvador@tecnico.ulisboa.pt>
2021-03-26 15:53:13 +0000

    python melhorado

--- Projeto1/python_solver/projeto.py
+++ Projeto1/python_solver/projeto.py
@@ -1,106 +1,68 @@
-# Python3 program to print DFS traversal
-# from a given given graph
-from collections import defaultdict
- 
-# This class represents a directed graph using
-# adjacency list representation
- 
-class Node:
-    def __init__(self, ident):
-        self.source = True
-        self.visited = False
-        
-
 class Graph:
-    # Constructor
+    class Node:
+        def __init__(self, ident):
+            self.source = True
+            self.visited = False
+            self.maxPath = 1
+
     def __init__(self, V):
- 
-        # default dictionary to store graph
-        self.graph = defaultdict(list)
-        self.topological_order = []
-        self.vertexs = [Node(i) for i in range(1,V+1)]
+        self.graph = [[] for i in range(V)]
+        self.vertexes = [Node(i) for i in range(1,V+1)]
 
-    # function to add an edge to graph
     def addEdge(self, u, v):
-        self.graph[u].append(v)
-        self.vertexs[v-1].source = False
- 
-    # A function used by DFS
-    # def explore(self, v, stack):
-    #     self.vertexs[v-1].visited = True
-    
-    #     for neighbour in self.graph[v]:
-    #         if not self.vertexs[neighbour-1].visited:
-    #             self.DFSUtil(neighbour)
-    #     self.topological_order = [v] + self.topological_order
- 
-    # def DFS(self, v):
-    #     stack = []
-    #     for i in range(len(self.vertexs)):
-    #         if not self.vertexs[i+1].visited:
-    #             for child in self.vertexs[i+1]:
-    #                 stack = [child] + stack
-    #             self.explore
+        self.graph[u-1].append(v-1)
+        self.vertexes[v-1].source = False
 
+    def getTopologicalOrder(self):
+        topologicalOrder = []
+        for i in range(len(self.vertexes)):
+            if not self.vertexes[i].visited:
+                topologicalOrder = self.explore(i) + topologicalOrder
+        return topologicalOrder
+    
     def explore(self, v):
         stack = [v]
-
+        topologicalOrder = []
         while stack:
             vi = stack[-1]
 
-            self.vertexs[vi - 1].visited = True
+            self.vertexes[vi].visited = True
 
             closed = True
             for n in self.graph[vi]:
-                if not self.vertexs[n - 1].visited:
+                if not self.vertexes[n].visited:
                     closed = False
                     stack.append(n)
 
             if closed:
                 stack.pop()
-                self.topological_order = [vi] + self.topological_order
- 
-    def DFS(self):
-        for i in range(len(self.vertexs)):
-            if not self.vertexs[i].visited:
-                self.explore(i + 1)
+                topologicalOrder = [vi] + topologicalOrder
+        
+        return topologicalOrder
     
- 
-# Driver code
+# Process input
 file = open("graph.in", "r")
 lines = file.read().splitlines()
 settings = lines[0].split()
 V = int(settings[0])
 E = int(settings[1])
 
- 
-# Create a graph given
-# in the above diagram
+# Initialize graph
 g = Graph(V)
 for i in range(E):
     splitted = lines[i+1].split()
     g.addEdge(int(splitted[0]), int(splitted[1]))
 
 
-# for i in range(len(g.vertexs)):
-#     if not g.vertexs[i].visited:
-#         g.DFS(i+1)
-g.DFS()
-
-mp = {}
-for el in g.topological_order:
-    mp[el] = 1
-for el in g.topological_order:
+maxPath = 1
+numberSources = 0
+for el in g.getTopologicalOrder():
+    if g.vertexes[el].source == True:
+        numberSources += 1
     for link in g.graph[el]:
-        if mp[el] + 1 > mp[link]:
-            mp[link] = mp[el] + 1
+        if g.vertexes[el].maxPath + 1 > g.vertexes[link].maxPath:
+            g.vertexes[link].maxPath = g.vertexes[el].maxPath + 1
+            if g.vertexes[link].maxPath > maxPath:
+                maxPath = g.vertexes[link].maxPath
     
-count = 0
-for el in g.vertexs:
-    if el.source == True:
-        count += 1
-
-print(count, max(mp.values()))
-
- 
-# This code is contributed by Neelam Yadav
+print(numberSources, maxPath)
